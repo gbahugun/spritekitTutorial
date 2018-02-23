4b4274dbb05adb0edfb3cc5fece66b66036b15ad
Gaganbir Bahugun <gaganbahugun@hotmail.com>
2018-02-23 11:39:47 -0800

    Added more setup

--- spritekitTutorial/spritekitTutorial/GameScene.swift
+++ spritekitTutorial/spritekitTutorial/GameScene.swift
@@ -11,23 +11,24 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    
+    
     override func didMove(to view: SKView) {
   
     }
     
-    func move(atPoint pos : CGPoint) {
+    func move(toPoint pos : CGPoint) {
 
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for t in touches { self.move(toPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for t in touches { self.move(toPoint: t.location(in: self)) }
     }
-    
+        
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
